3b84c932477b438a434ca5d501c0316fc963d588
Nastya <nastyash070806@gmail.com>
2025-11-08 12:12:13 +0200

    Додала відображення фото ПР-2

--- src/app/components/item-card/item-card.ts
+++ src/app/components/item-card/item-card.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './item-card.html',
   styleUrl: './item-card.css',
 })
